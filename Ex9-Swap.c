6a813aea04d08ee09eed2358bff696506f37e86e
leticia sousa <88945495+thzlet@users.noreply.github.com>
2025-04-09 20:50:06 -0300

    Create Ex9-Swap.c

---
+++ Ex9-Swap.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+void troca(int *a, int *b);
+
+void troca(int *a, int *b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+int main() {
+    int x = 5, y = 10;
+
+    printf("Antes da troca: x = %d, y = %d\n", x, y);
+    troca(&x, &y);
+    printf("Depois da troca: x = %d, y = %d\n", x, y);
+
+    return 0;
+}
